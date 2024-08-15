a2023beacb4b66f4aecbcc3ed6ba634a147b19ab
nhl1090 <neilhdz24@gmail.com>
2024-08-15 05:40:46 -0500

    Index.js updated. SVG file now able to be generated based on user input.

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {Circle, Triangle, Square, Rectangle} = require('./lib/shapes')
+const { Circle, Triangle, Square, Rectangle } = require('./lib/shapes');
 
 function promptUser() {
     inquirer.prompt([
         {
             type: 'input',
-            name:'text',
+            name: 'text',
             message: 'Enter up to 3 characters for your logo:',
             validate: input => input.length <= 3 || 'Value must not exceed 3 characters.'
         },
         {
             type: 'input',
             name: 'textColor',
-            message: 'Enter the text color (keyword or hexadecimal):',  
+            message: 'Enter the text color (keyword or hexadecimal):',
         },
         {
             type: 'list',
@@ -28,11 +28,40 @@ function promptUser() {
         },
     ])
     .then(answers => {
-        if (answers.text.length > 3) {
-            console.log("Value must not exceed 3 characters");
-            promptQuestions();
-        } else {
-            console.log('Your logo.svg was successfully generated!')
+        let shape;
+
+        switch (answers.shape) {
+            case 'Circle':
+                shape = new Circle(answers.shapeColor);
+                break;
+            case 'Triangle':
+                shape = new Triangle(answers.shapeColor);
+                break;
+            case 'Square':
+                shape = new Square(answers.shapeColor);
+                break;
+            case 'Rectangle':
+                shape = new Rectangle(answers.shapeColor);
+                break;
         }
+
+        const svgContent = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">
+                ${shape.render()}
+                <text x="150" y="125" font-size="60" text-anchor="middle" fill="${answers.textColor}">
+                    ${answers.text}
+                </text>
+            </svg>
+        `;
+
+        fs.writeFile('logo.svg', svgContent.trim(), err => {
+            if (err) {
+                console.error('Error generating logo.svg:', err);
+            } else {
+                console.log('Your logo.svg was successfully generated!');
+            }
         });
+    });
 }
+
+promptUser();
